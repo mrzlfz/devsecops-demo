1ead327af43efcce87d6082609a5f36057d2fc55
myrizalfauzi <rizal@rizalfauzi.me>
2022-07-15 23:35:22 +0700

    update

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -48,11 +48,6 @@ public class NumericApplicationTests {
     
     @Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
-    }
-    
-    @Test
-    public void welcomeMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Kubernetes DevSecOps"));
     }
