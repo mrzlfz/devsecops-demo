309284dc67b78edf9e59422758f768fbeb3dbd6f
myrizalfauzi <rizal@rizalfauzi.me>
2022-07-20 20:26:26 +0700

    update websecurt

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -2,14 +2,23 @@ package com.devsecops;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http.csrf().disable();
+
+    // }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     
         http.csrf().disable();
-
+        return http.build();
     }
 }
