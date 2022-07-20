b80f8c387b9f2a59fc4bfe80b4f8809e436b0262
myrizalfauzi <rizal@rizalfauzi.me>
2022-07-20 22:05:50 +0700

    update

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.devsecops;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 // import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 public class WebSecurityConfig {
 
     // @Override
@@ -16,9 +14,9 @@ public class WebSecurityConfig {
     // }
 
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
      
-        http.csrf().disable();
+        // http.csrf().disable();
         return http.build();
     }
 }
